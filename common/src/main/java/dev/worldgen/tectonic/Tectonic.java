7157feb94303c6667acdc70d85f609b500779d75
Caimee <witty8010@outlook.com>
2026-02-09 03:16:24 +0800

    Changed configure of internal terratonic datapacks

---
+++ common/src/main/java/dev/worldgen/tectonic/Tectonic.java
@@ -0,0 +1,20 @@
+package dev.worldgen.tectonic;
+
+import dev.worldgen.tectonic.config.ConfigHandler;
+import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+
+public class Tectonic {
+    public static final String MOD_ID = "tectonic";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static void init(Path path) {
+        ConfigHandler.load(path);
+    }
+
+    public static ResourceLocation id(String name) {
+        return new ResourceLocation("tectonic", name);
+    }
+}
